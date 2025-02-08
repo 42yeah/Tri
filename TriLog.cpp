cd075148d96cafe58ed1b6b967a1fc24bc296679
42yeah <hey@42yeah.is>
2025-02-09 01:32:05 +0800

    Colored logs and options to disable them

--- TriLog.cpp
+++ TriLog.cpp
@@ -1,38 +1,66 @@
 #include "TriLog.hpp"
 
+#if TRI_COLORED_LOG
+#define TRI_COLOR_VERBOSE "\x1b[90m"
+#define TRI_COLOR_INFO "\x1b[92m"
+#define TRI_COLOR_WARNING "\x1b[33m"
+#define TRI_COLOR_ERROR "\x1b[91m"
+#define TRI_COLOR_RESET "\x1b[0m"
+
+#else
+#define TRI_COLOR_VERBOSE ""
+#define TRI_COLOR_INFO ""
+#define TRI_COLOR_WARNING ""
+#define TRI_COLOR_ERROR ""
+#define TRI_COLOR_RESET ""
+
+#endif
+
 // Verbose
 
 template <> void TriLogger<TriLogVerbose>::Tag()
 {
-    std::cout << "[" << TRI_LOG_APP << "] [D] ";
+    std::cout << TRI_COLOR_VERBOSE << "[" << TRI_LOG_APP << "] [D] ";
 }
 
-template <> void TriLogger<TriLogVerbose>::Flush() { std::cout << std::endl; }
+template <> void TriLogger<TriLogVerbose>::Flush()
+{
+    std::cout << TRI_COLOR_RESET << std::endl;
+}
 
 // Info
 
 template <> void TriLogger<TriLogInfo>::Tag()
 {
-    std::cout << "[" << TRI_LOG_APP << "] [I] ";
+    std::cout << TRI_COLOR_INFO << "[" << TRI_LOG_APP << "] [I] ";
 }
 
-template <> void TriLogger<TriLogInfo>::Flush() { std::cout << std::endl; }
+template <> void TriLogger<TriLogInfo>::Flush()
+{
+    std::cout << TRI_COLOR_RESET << std::endl;
+}
 
 // Warning
 
 template <> void TriLogger<TriLogWarning>::Tag()
 {
-    std::cerr << "[" << TRI_LOG_APP << "] [W] ";
+    std::cerr << TRI_COLOR_WARNING << "[" << TRI_LOG_APP << "] [W] ";
 }
 
-template <> void TriLogger<TriLogWarning>::Flush() { std::cerr << std::endl; }
+template <> void TriLogger<TriLogWarning>::Flush()
+{
+    std::cerr << TRI_COLOR_RESET << std::endl;
+}
 
 // Error
 
 template <> void TriLogger<TriLogError>::Tag()
 {
-    std::cout << "[" << TRI_LOG_APP << "] [E] ";
+    std::cout << TRI_COLOR_ERROR << "[" << TRI_LOG_APP << "] [E] ";
 }
 
-template <> void TriLogger<TriLogError>::Flush() { std::cerr << std::endl; }
+template <> void TriLogger<TriLogError>::Flush()
+{
+    std::cerr << TRI_COLOR_RESET << std::endl;
+}
 
